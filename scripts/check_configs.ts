c1e22d3a7552034d95380fb81f7026c3e33145a5
Thong Yuan Yu Sarah <65583522+sarah-thong@users.noreply.github.com>
2024-12-13 10:19:08 +0800

    Feat/xfer disabled tokens (#284)
    * Add transfer disabled tokens field
    
    * Add token name overrides map

--- scripts/check_configs.ts
+++ scripts/check_configs.ts
@@ -12,6 +12,8 @@ interface ConfigJSON {
   blacklisted_markets: string[];
   blacklisted_pools: string[];
   blacklisted_tokens: string[];
+  transfer_disabled_tokens: TransferDisabledTokensObj;
+  token_name_override_map: TokenNameOverrideMap;
   transfer_options: {
     [chainKey: string]: number;
   };
@@ -55,6 +57,15 @@ interface PerpPoolBanner {
   action_trigger_date?: string;
   past_tense_text?: string;
   subtext?: string;
+}
+
+interface TransferDisabledTokensObj {
+  deposit: string[];
+  withdraw: string[];
+}
+
+type TokenNameOverrideMap = {
+  [denom: string]: string;
 }
 
 interface DemexPointsConfig {
@@ -180,6 +191,16 @@ function checkAddressIsEVM(address: string): Boolean {
   return regex.test(address)
 }
 
+function isErrorOutcome(outcome: DuplicateEntry): boolean {
+  return Boolean(outcome.status && outcome.entry?.length && outcome.entry.length > 0);
+}
+
+function joinEntriesIntoStr(entriesArr: string[]): string {
+  return entriesArr.length > 1
+    ? `${entriesArr.slice(0, -1).join(", ")} and ${entriesArr[entriesArr.length - 1]}`
+    : entriesArr[0];
+}
+
 // check list of markets to ensure that it does not have blacklisted markets 
 function checkBlacklistedMarkets(marketData: string[], blacklistedMarkets: string[]): InvalidEntry {
   let overlappingMarkets: string[] = [];
@@ -194,6 +215,50 @@ function checkBlacklistedMarkets(marketData: string[], blacklistedMarkets: strin
   } : {
     status: false
   };
+}
+
+function isValidTransferDisabledTokens(transferDisabledTokens: TransferDisabledTokensObj, denoms: string[], network: CarbonSDK.Network): boolean {
+  const dupDepositTknsOutcome = checkDuplicateEntries(transferDisabledTokens.deposit);
+  const dupWithdrawTknsOutcome = checkDuplicateEntries(transferDisabledTokens.withdraw);
+
+  if (dupDepositTknsOutcome.status || dupWithdrawTknsOutcome.status) {
+    if (isErrorOutcome(dupDepositTknsOutcome)) {
+      const duplicateDepositTokensStr = joinEntriesIntoStr(dupDepositTknsOutcome.entry!);
+      console.error(`[ERROR] transfer_disabled_tokens.deposit of ${network}.json has the following duplicate token denoms: ${duplicateDepositTokensStr}. Please make sure to input each denom only once.`);
+    }
+    if (isErrorOutcome(dupWithdrawTknsOutcome)) {
+      const duplicateWithdrawTokensStr = joinEntriesIntoStr(dupWithdrawTknsOutcome.entry!);
+      console.error(`[ERROR] transfer_disabled_tokens.withdraw of ${network}.json has the following duplicate token denoms: ${duplicateWithdrawTokensStr}. Please make sure to input each denom only once.`);
+    }
+    return false;
+  }
+
+  const validDepositTknsOutcome = checkValidEntries(transferDisabledTokens.deposit, denoms);
+  const validWithdrawTknsOutcome = checkValidEntries(transferDisabledTokens.withdraw, denoms);
+  if (validDepositTknsOutcome.status || dupWithdrawTknsOutcome.status) {
+    if (isErrorOutcome(validDepositTknsOutcome)) {
+      const invalidDepositTokensStr = joinEntriesIntoStr(validDepositTknsOutcome.entry!);
+      console.error(`[ERROR] transfer_disabled_tokens.deposit of ${network}.json has the following invalid token denoms: ${invalidDepositTokensStr}. Please make sure to input only valid token denoms.`);
+    }
+    if (isErrorOutcome(validWithdrawTknsOutcome)) {
+      const invalidWithdrawTokensStr = joinEntriesIntoStr(validWithdrawTknsOutcome.entry!);
+      console.error(`[ERROR] transfer_disabled_tokens.withdraw of ${network}.json has the following invalid token denoms: ${invalidWithdrawTokensStr}. Please make sure to input only valid token denoms.`);
+    }
+    return false;
+  }
+
+  return true;
+}
+
+function isValidTokenNameOverrideMap(tokenNameOverrideMap: TokenNameOverrideMap, denoms: string[], network: CarbonSDK.Network): boolean {
+  const denomKeysArr = Object.keys(tokenNameOverrideMap);
+  const invalidDenomsOutcome = checkValidEntries(denomKeysArr, denoms);
+  if (isErrorOutcome(invalidDenomsOutcome)) {
+    const invalidTokensStr = joinEntriesIntoStr(invalidDenomsOutcome.entry!);
+    console.error(`[ERROR] token_name_override_map of ${network}.json has the following invalid token denom keys: ${invalidTokensStr}. Please make sure to input only valid token denoms.`);
+    return false;
+  }
+  return true;
 }
 
 function isValidExternalChainChannels(chainChannels: ExternalChannelsObj, bridges: string[], network: CarbonSDK.Network): boolean {
@@ -475,6 +540,14 @@ async function main() {
         console.error(`ERROR: ${network}.json has the following duplicated blacklisted token denom entries: ${listOfDuplicates}. Please make sure to input each token denom only once in ${network}`);
         outcomeMap[network] = false;
       }
+
+      // transfer disabled tokens object check
+      const isTransferDisabledTokensValid = isValidTransferDisabledTokens(jsonData.transfer_disabled_tokens, tokens, network);
+      if (!isTransferDisabledTokensValid) outcomeMap[network] = false;
+
+      // token_name_override_map check
+      const isTokenNameOverrideMapValid = isValidTokenNameOverrideMap(jsonData.token_name_override_map, tokens, network);
+      if (!isTokenNameOverrideMapValid) outcomeMap[network] = false;
 
       const hasInvalidCrossSellingTokens = checkValidEntries(jsonData.cross_selling_source_tokens, tokens);
       if (hasInvalidCrossSellingTokens.status && hasInvalidCrossSellingTokens.entry) {
